65caf419c9888e6a13313a9c71266014d89a1846
paulojfernandes <paulojdf@sapo.pt>
2017-05-17 21:57:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,12 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 //Defining middleware to serve static files
 app.use('/static', express.static('public'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/home', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/index.html'));
     if (req.session.username == null & req.session.password == null) {
