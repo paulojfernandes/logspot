ef5469c6762812ec5edc4fc502cbe1c25557befa
paulojfernandes <paulojdf@sapo.pt>
2017-05-17 21:54:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ var app = express();
 
 /*var cors = require('cors')
 app.use(cors())*/
-app.use(function(req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-})
 
 app.use(require('./controllers/index'))
 
