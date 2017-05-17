3994a33cb968aa44cc5c08882f3670acf0157f13
paulojfernandes <paulojdf@sapo.pt>
2017-05-17 21:51:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ var http = require('http');
 var exphbs = require('express-handlebars');
 var app = express();
 
-var cors = require('cors')
-app.use(cors())
+/*var cors = require('cors')
+app.use(cors())*/
+app.use(function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+})
 
 app.use(require('./controllers/index'))
 
